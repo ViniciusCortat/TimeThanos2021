c676485047fda7d15421c462936584abfeb07ed1
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2021-01-29 19:56:23 -0300

    PointSystem

---
+++ TimeThanos - 2021/Assets/Scripts/PointSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class PointSystem
+{
+    // Criando Singleton
+    private static PointSystem instance = null;
+    private static readonly object padlock = new object();
+    PointSystem() {}
+    public static PointSystem Instance {get {lock(padlock){if(instance==null){instance = new PointSystem();}return instance;}}}
+
+    // Fim da criacao do Singleton
+
+    private int _pontos;
+
+    public int pontos {get {return _pontos;}}
+
+    public void GivePonto(int give) {
+        _pontos += give;
+    }
+
+    public void ResetPonto() {
+        _pontos = 0;
+    }
+
+
+}
