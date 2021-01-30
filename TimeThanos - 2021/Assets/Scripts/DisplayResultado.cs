1c73d0f3adf142431ed3fbe8bfc9686ee1b2ed10
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2021-01-30 01:04:10 -0300

    Ordem certa

--- TimeThanos - 2021/Assets/Scripts/DisplayResultado.cs
+++ TimeThanos - 2021/Assets/Scripts/DisplayResultado.cs
@@ -30,6 +30,7 @@ public class DisplayResultado : MonoBehaviour
         Hs.Scores.Add(Points.pontos);
         SaveSystem.GetInstance().SaveHighScore();
         Hs.Scores.Sort();
+        Hs.Scores.Reverse();
         int max = Hs.Scores.Count > 10? 10 : Hs.Scores.Count;
         for(int i = 0;i < max; i++) {
             score.text += $"Rank {i+1} - {Hs.Scores[i]}\n";
