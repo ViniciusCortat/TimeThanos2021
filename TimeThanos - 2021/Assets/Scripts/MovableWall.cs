f17319e323883135a9f8d3fe8936eba904906c6f
Hugo Tonette <hugo.tonette@gmail.com>
2021-01-29 21:29:19 -0300

    Paredes se movem
    * Prototipo do labirinto
    * Paredes se movem

---
+++ TimeThanos - 2021/Assets/Scripts/MovableWall.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableWall : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject Wall;
+	[SerializeField]
+	private Transform StartPoint;
+	[SerializeField]
+	private Transform FinalPoint;
+
+	[SerializeField]
+	private float WaitTime = 30f;
+
+	private bool _isActive = false;
+
+	private void Start()
+	{
+		InvokeRepeating("ChoosePoint", WaitTime, WaitTime);
+	}
+
+	private void ChoosePoint()
+	{
+		if (_isActive)
+		{
+			StartCoroutine(MoveWall(FinalPoint.position, 2));
+		}
+		else
+		{
+			StartCoroutine(MoveWall(StartPoint.position, 2));
+		}
+
+	}
+
+	IEnumerator MoveWall(Vector3 targetPosition, float duration)
+	{
+		float time = 0;
+		Vector3 startPosition = Wall.transform.position;
+
+		while (time < duration)
+		{
+			Wall.transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+			time += Time.deltaTime;
+			yield return null;
+		}
+		Wall.transform.position = targetPosition;
+
+		_isActive = !_isActive;
+	}
+}
