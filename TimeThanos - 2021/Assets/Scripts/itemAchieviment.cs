b71699ca72ed219e7352191e73cba61b51a3199e
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2021-01-31 18:02:22 -0300

    som achivimentes

--- TimeThanos - 2021/Assets/Scripts/itemAchieviment.cs
+++ TimeThanos - 2021/Assets/Scripts/itemAchieviment.cs
@@ -22,56 +22,83 @@ public class itemAchieviment : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Map")) {
-            Achiev.AddMaps();
-            Destroy(other.gameObject);
+            //Achiev.AddMaps();
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Book")) {
-            Achiev.AddBook();
-            Destroy(other.gameObject);
+            //Achiev.AddBook();
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Plant")) {
-            Achiev.AddPlant();
-            Destroy(other.gameObject);
+            //Achiev.AddPlant();
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Fireplace")) {
-            GM.TriggerAchieviment("Fireplace");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("Fireplace");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Familiar")) {
-            GM.TriggerAchieviment("Familiar");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("Familiar");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Skull")) {
-            GM.TriggerAchieviment("Skull");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("Skull");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Ring")) {
-            GM.TriggerAchieviment("Ring");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("Ring");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Mirror")) {
-            GM.TriggerAchieviment("Mirror");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("Mirror");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Statue")) {
-            GM.TriggerAchieviment("Statue");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("Statue");
+           //Destroy(other.gameObject);
+           PlayAudioAndDisable(other);
         }
         if(other.CompareTag("OwlTrophy")) {
-            GM.TriggerAchieviment("OwlTrophy");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("OwlTrophy");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Hat")) {
-            GM.TriggerAchieviment("Hat");
-            Destroy(other.gameObject);
+           // GM.TriggerAchieviment("Hat");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Broom")) {
-            GM.TriggerAchieviment("Broom");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("Broom");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
         }
         if(other.CompareTag("Cauldron")) {
-            GM.TriggerAchieviment("Cauldron");
-            Destroy(other.gameObject);
+            //GM.TriggerAchieviment("Cauldron");
+            //Destroy(other.gameObject);
+            PlayAudioAndDisable(other);
+        }
+    }
+
+    private void PlayAudioAndDisable(Collider other)
+    {
+
+        GameObject obj = other.gameObject;
+        obj.GetComponent<BoxCollider>().enabled = false;
+        foreach (Transform child in obj.transform)
+        {
+            if (child.name == "audio")
+                child.gameObject.GetComponent<AudioSource>().Play();
+            else
+                child.gameObject.SetActive(false);
         }
     }
 }
