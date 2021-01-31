942408857b1bb3454d99a060eab10f3c4f93d5fa
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2021-01-31 16:43:47 -0300

    Revert "asefasef"
    This reverts commit 43366fd58c0ed2e0a7e3cbb5b73db2b3cd1a9ace.

--- TimeThanos - 2021/Assets/Scripts/AudioManager.cs
+++ TimeThanos - 2021/Assets/Scripts/AudioManager.cs
@@ -78,7 +78,7 @@ public class AudioManager : MonoBehaviour
 
     IEnumerator WaitToPlay()
     {
-        yield return new WaitForSeconds(6.0f);
+        yield return new WaitForSeconds(3.0f);
         tema.Play();
 
     }
