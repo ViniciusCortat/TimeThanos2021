1633dbe82dc53b9855756f302d79d68b8bcf7a3d
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2021-02-08 02:34:40 -0300

    Outfit 100%?

--- TimeThanos - 2021/Assets/Scripts/PlayerHat.cs
+++ TimeThanos - 2021/Assets/Scripts/PlayerHat.cs
@@ -6,33 +6,108 @@ public class PlayerHat : MonoBehaviour
 {
     public List<GameObject> HatList;
     private Hats hats;
-    private List<GameObject> CaveiraEquipped;
+
+    private GameObject Cabelo_inteiro;
+    private GameObject Cabelo_3;
+    private GameObject Oculos;
+    private GameObject Cabelo_2;
+    private GameObject Franja;
+    private GameObject Chapeu;
 
     void Start()
     {
         hats = SaveSystem.GetInstance().hats;
-        CaveiraEquipped = new List<GameObject>();
-        CaveiraEquipped.Add(gameObject.transform.GetChild(0).GetChild(2).gameObject);
-        CaveiraEquipped.Add(gameObject.transform.GetChild(0).GetChild(3).gameObject);
-        CaveiraEquipped.Add(gameObject.transform.GetChild(0).GetChild(4).gameObject);
-        CaveiraEquipped.Add(gameObject.transform.GetChild(0).GetChild(5).gameObject);
+        Cabelo_inteiro = gameObject.transform.GetChild(0).GetChild(2).gameObject;
+        Cabelo_2 = gameObject.transform.GetChild(0).GetChild(3).gameObject;
+        Cabelo_3 = gameObject.transform.GetChild(0).GetChild(4).gameObject;
+        Chapeu = gameObject.transform.GetChild(0).GetChild(7).gameObject;
+        Franja = gameObject.transform.GetChild(0).GetChild(9).gameObject;
+        Oculos = gameObject.transform.GetChild(0).GetChild(10).gameObject;
         ActivateHat();
-        
     }
 
     public void ActivateHat() {
         for(int i=0;i<HatList.Count;i++) {
             HatList[i].SetActive(false);
         }
-        for(int i=0;i<CaveiraEquipped.Count;i++) {
-            CaveiraEquipped[i].SetActive(true);
+        CheckHatCondition();
+        if(hats.CurrentHat() != -1) {
+            HatList[hats.CurrentHat()].SetActive(true);
         }
-        HatList[hats.CurrentHat()].SetActive(true);
-        if(hats.CurrentHat() == 4) {
-            for(int i=0;i<CaveiraEquipped.Count;i++) {
-                CaveiraEquipped[i].SetActive(false);
-            }
-        }
+    }
 
+    public void CheckHatCondition() {
+        int hn = hats.CurrentHat();
+        Cabelo_inteiro.SetActive(true);
+        Cabelo_3.SetActive(true);
+        Oculos.SetActive(true);
+        Chapeu.SetActive(false);
+        Cabelo_2.SetActive(false);
+        Franja.SetActive(false);
+
+        if(hn == 0) {
+            Cabelo_inteiro.SetActive(false);
+            Cabelo_3.SetActive(true);
+            Oculos.SetActive(true);
+            Chapeu.SetActive(true);
+            Cabelo_2.SetActive(true);
+            Franja.SetActive(true);
+        }
+        if(hn == 4) {
+            Cabelo_inteiro.SetActive(true);
+            Oculos.SetActive(true);
+            Chapeu.SetActive(false);
+            Cabelo_2.SetActive(false);
+            Cabelo_3.SetActive(false);
+            Franja.SetActive(false);
+        }
+        if(hn == 6 || hn == 20 || hn == 22) {
+            Chapeu.SetActive(false);
+            Cabelo_2.SetActive(false);
+            Cabelo_3.SetActive(false);
+            Oculos.SetActive(false);
+            Franja.SetActive(false);
+            Cabelo_inteiro.SetActive(false);
+        }
+        if(hn == 10 || hn == 19 || hn == 21) {
+            Chapeu.SetActive(true);
+            Cabelo_2.SetActive(true);
+            Cabelo_3.SetActive(true);
+            Oculos.SetActive(true);
+            Franja.SetActive(true);
+            Cabelo_inteiro.SetActive(false);
+        }
+        if(hn == 13) {
+            Cabelo_inteiro.SetActive(true);
+            Cabelo_3.SetActive(true);
+            Chapeu.SetActive(false);
+            Cabelo_2.SetActive(false);
+            Franja.SetActive(false);
+            Oculos.SetActive(false);
+        }
+        if(hn == 16 || hn == 17 || hn == 23) {
+            Chapeu.SetActive(false);
+            Cabelo_2.SetActive(true);
+            Cabelo_3.SetActive(true);
+            Oculos.SetActive(true);
+            Franja.SetActive(true);
+            Cabelo_inteiro.SetActive(false);
+        }
+        if(hn == 18) {
+            Cabelo_inteiro.SetActive(true);
+            Cabelo_3.SetActive(true);
+            Oculos.SetActive(true);
+            Chapeu.SetActive(false);
+            Cabelo_2.SetActive(false);
+            Franja.SetActive(false);
+        }
+        if(hn == 24) {
+            Cabelo_3.SetActive(true);
+            Oculos.SetActive(true);
+            Chapeu.SetActive(false);
+            Cabelo_2.SetActive(false);
+            Franja.SetActive(false);
+            Cabelo_inteiro.SetActive(false);
+        }
     }
 }
